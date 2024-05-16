4ccd08df415fc55da92968aa7f27f3f34326d321
Oak Arboleda <oak.arboleda@sharedcreatives.com>
2024-05-16 14:16:59 -0500

    Fix Building error

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,23 +1,14 @@
 import Heading from '@/app/portfolio/Heading';
 import More from '@/app/portfolio/More';
-import Page from '@/app/components/Page';
 import React from 'react';
 import AllProjects from '@/app/portfolio/allProjects';
 
 export function Portfolio({ ...props }) {
   return (
-    <Page
-      currentPage="Portfolio"
-      meta={{
-        title: 'Oak Arboleda - Senior Software Developer',
-        desc: 'YO!',
-      }}
-    >
-      <div className="container top-0 w-full bg-slate-900">
-        <Heading />
-        <AllProjects />
-        <More />
-      </div>
-    </Page>
+    <div className="container top-0 w-full bg-slate-900">
+      <Heading />
+      <AllProjects />
+      <More />
+    </div>
   );
 }
